2c9ce75eb69f1285808cc3ef50e3a5cb43c2488c
Parthita <90266945+Parthita@users.noreply.github.com>
2023-01-05 18:40:33 +0530

    Add files via upload

---
+++ cyber-city/theme.bash
@@ -0,0 +1,92 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2022 Aditya Shakya <adi1090x@gmail.com>
+#
+# Beach Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#030315'
+foreground='#a57ff5'
+color0='#1b1f45'
+color1='#383eeb'
+color2='#6654de'
+color3='#3311a6'
+color4='#2587cc'
+color5='#696ebf'
+color6='#ed2b3e'
+color7='#f00a21'
+color8='#4a697d'
+color9='#fa3935'
+color10='#18c8db'
+color11='#d9a400'
+color12='#2ca2f5'
+color13='#8086e8'
+color14='#33c5ba'
+color15='#fdf6e3'
+
+accent='#5294E2'
+light_value='0.03'
+dark_value='0.80'
+
+# Wallpaper
+wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wallpaper="$wdir/wallpaper"
+
+# Polybar
+polybar_font='Iosevka Nerd Font:size=10;3'
+
+# Rofi
+rofi_font='Iosevka 10'
+rofi_icon='Numix-Apps'
+
+# Terminal
+terminal_font_name='JetBrainsMono Nerd Font'
+terminal_font_size='10'
+
+# Geany
+geany_colors='keyboards.conf'
+geany_font='JetBrains Mono 10'
+
+# Appearance
+gtk_font='Noto Sans 9'
+gtk_theme='Cyberpunk'
+icon_theme='candy-icons'
+cursor_theme='Sweet'
+
+# Openbox
+ob_theme='Sweet-Dark'
+ob_layout='CLM'
+ob_font='JetBrains Mono'
+ob_font_size='9'
+ob_menu='menu-icons.xml'
+ob_margin_t='50'
+ob_margin_b='10'
+ob_margin_l='10'
+ob_margin_r='10'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='20x58'
+dunst_origin='bottom-right'
+dunst_font='Iosevka Custom 9'
+dunst_border='0'
+dunst_separator='2'
+
+# Plank
+plank_hmode='auto'
+plank_offset='0'
+plank_position='bottom'
+plank_theme='Transparent'
+plank_icon_size='40'
+plank_zoom_percent='120'
+
+# Picom
+picom_backend='glx'
+picom_corner='14'
+picom_shadow_r='14'
+picom_shadow_o='0.30'
+picom_shadow_x='-12'
+picom_shadow_y='-12'
+picom_blur_method='none'
+picom_blur_strength='8'
